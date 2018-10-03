ab550b46523bab2be23f8a880b85239f35e54f65
Unknown <leo.salemann@me.com>
2018-10-02 21:33:17 -0700

    End of class checkin, using online editor.

--- Class01/Blink/Blink.ino
+++ Class01/Blink/Blink.ino
@@ -26,12 +26,15 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
+  Serial.println("it's up");
+  delay(1500);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
+  Serial.println("it's down");
+  delay(1500);                       // wait for a second
 }
