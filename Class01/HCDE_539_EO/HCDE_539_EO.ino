33663eab5340d7c70cb7658efaa4766a8683082c
Unknown <leo.salemann@me.com>
2018-10-07 16:33:02 -0700

    Fixed some port ptoblems

--- Class01/HCDE_539_EO/HCDE_539_EO.ino
+++ Class01/HCDE_539_EO/HCDE_539_EO.ino
@@ -8,10 +8,15 @@
  * link: https://www.tinkercad.com/things/dTAcB2IUt0I-editing-components/editel?collectionid=OIYJ88OJ3OPN3EA&lessonid=EFU6PEHIXGFUR1J&projectid=OIYJ88OJ3OPN3EA#/lesson-viewer
  *****************************************/
 
-
-int led_red = 0; // the red LED is connected to Pin 0 of the Arduino
-int led_yellow = 1; // the yellow LED is connected to Pin 1 of the Arduino
-int led_green = 2; // the green LED is connected to Pin 2 of the Arduino
+/*
+ * In TinkerCad, the pins are 0, 1, 2 but that causes an error:
+ *   avrdude stk500_getsync() not in sync resp=0x30
+ * After much googling, I found a solution that suggested to avoid using pin 0
+ *   https://www.instructables.com/id/A-solution-to-avrdude-stk500getsync-not-in-syn/
+ */
+int led_red = 1;    // the red    LED is connected to Pin 1 of the Arduino
+int led_yellow = 2; // the yellow LED is connected to Pin 2 of the Arduino
+int led_green = 3;  // the green  LED is connected to Pin 3 of the Arduino
 
 void setup() {
   // set up all the LEDs as OUTPUT
