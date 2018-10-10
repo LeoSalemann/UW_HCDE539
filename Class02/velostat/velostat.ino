66b8c604ed39d6c37959e40e4e1e76f9a475321f
Unknown <leo.salemann@me.com>
2018-10-09 21:44:20 -0700

    Adde temperatur & velostat. Endof class commit

---
+++ Class02/velostat/velostat.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensor = analogRead(A0);
+  Serial.println(sensor);
+  delay(10);
+}
