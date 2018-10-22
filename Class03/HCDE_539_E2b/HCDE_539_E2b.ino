f5d2254efcbcfb396f2092d67d2063313b1daff9
Unknown <leo.salemann@me.com>
2018-10-21 18:35:07 -0700

    MAJOR REFACTOR, start with TinkerCAD state change detection demo.
    Straight copy, no pin changes or nothing.  Got it from tinkercad; they say it's from http://www.arduino.cc/en/Tutorial/ButtonStateChange

--- Class03/HCDE_539_E2b/HCDE_539_E2b.ino
+++ Class03/HCDE_539_E2b/HCDE_539_E2b.ino
@@ -1,73 +1,74 @@
-/****************************************
- * Leo Salemann
- * HCDE 539 Physical Computing
- * University of Washinton
- * Assignment E2 Integrated Builds part B: Doorbell
- * 
- * Sources: 
- *   UW buzzing Demo
- *   https://www.arduino.cc/reference/en/language/functions/advanced-io/tone/
- *****************************************/
- 
- #include <Button.h>
+/*
 
-const int ledPin = LED_BUILTIN; // substitute 13 if using an old version of Arduino
-const int buttPin = 4;
-const int buzzPin = 8;
+  State change detection (edge detection)
 
-Button butt (buttPin);
+  Often, you don't need to know the state of a
+  digital input all the time,  but you just need
+  to know when the input changes from one state to
+  another.  For example, you want to know when a
+  button goes from OFF to ON.  This is called
+  state change detection, or edge detection.
 
-boolean buzzing = false;
-int milliseconds = 0;
+  This example shows how to detect when a button
+  or button changes from off to on  and on to off.
+
+
+  The circuit:
+  * pushbutton attached to pin 2 from +5V
+  * 10K resistor attached to pin 2 from ground
+  * LED attached from pin 13 to ground (or use the
+  built-in LED on  most Arduino boards)
+
+  This example code is in the public domain.
+  http://www.arduino.cc/en/Tutorial/ButtonStateChange
+*/
+
+int buttonState = 0;
+
+int lastButtonState = 0;
+
+int buttonPushCounter = 0;
 
 void setup()
 {
-  // housekeeping
+  pinMode(2, INPUT);
   Serial.begin(9600);
-  Serial.println("starting button demo");
-  pinMode(ledPin, OUTPUT);
-}
 
-/*
- * Funny story here: I had mispelled milliseconds in the argument spec,
- * using one l isntead of two.  So when I tried pressing the buttion, the 
- * delay was always zero, and I never got a tone.
- */
-void playTune (int milliseconds){
-  // buzzing = true;
-  digitalWrite(ledPin, HIGH);
-  tone (buzzPin, 400, milliseconds);  
-  Serial.println("buzz...");
-  
-  delay(milliseconds);
-  
-  digitalWrite(ledPin, LOW);
-  noTone(buzzPin);
-  Serial.println("...off");
+  pinMode(13, OUTPUT);
 }
-
 
 void loop()
 {
-  
-  int action = butt.checkButtonAction();
-
-  // if clicked, start the tone
-  if (action == Button::CLICKED) {
-    Serial.println("on");
-
+  // read the pushbutton input pin
+  buttonState = digitalRead(2);
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH, then the button
+      // went from off to on
+      buttonPushCounter += 1;
+      Serial.println("on");
+      Serial.print("number of button pushes: ");
+      Serial.println(buttonPushCounter);
+    } else {
+      // if the current state is LOW, then the button
+      // went from on to off
+      Serial.println("off");
+    }
+    // delay a little bit to avoid debouncing
+    delay(5); // Wait for 5 millisecond(s)
   }
-  // if held, stop the tone
-  if (action == Button::HELD_CLICKED) {
-    Serial.println("off");
+  // save the current state as the last state, for
+  // the next time through the loop
+  lastButtonState = buttonState;
+  // turns on the LED every four button pushes by
+  // checking the modulo of the button push counter.
+  // the modulo function gives you the remainder of
+  // the devision of two numbers
+  if (buttonPushCounter % 4 == 0) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
   }
- 
- // if (action == Button::PRESSED) {
- 
- /*   
-  if (action == Button::HELD_CLICKED) {
-    Serial.println("pressed");
-    playTune(1000);
-  } 
-  */
 }
