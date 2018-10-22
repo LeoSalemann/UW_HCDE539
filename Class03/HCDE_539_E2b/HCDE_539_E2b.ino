84ee73b39dd5d1e624f3d6c6bd273c93bdb2ecf4
Unknown <leo.salemann@me.com>
2018-10-21 18:48:24 -0700

    Added the buzzer part

--- Class03/HCDE_539_E2b/HCDE_539_E2b.ino
+++ Class03/HCDE_539_E2b/HCDE_539_E2b.ino
@@ -6,6 +6,13 @@
  * 
  * Sources: 
  *   Tinkercad stae change detection demo
+ *   http://www.arduino.cc/en/Tutorial/ButtonStateChange
+ *
+ *   Arduino tone() function
+ *   https://www.arduino.cc/reference/en/language/functions/advanced-io/tone/
+ *
+ *   Adafruit Piezzo Buzzer docs
+ *   https://www.adafruit.com/product/160
  *****************************************/
  
 /*
@@ -40,22 +47,34 @@ int lastButtonState = 0;
 int buttonPushCounter = 0;
 
 int pinButton = 4;
-int pinLED = 13;
+int pinBuzzer = 8;
+
+bool firstTime = true;
 
 void setup()
 {
   pinMode(pinButton, INPUT);
   Serial.begin(9600);
 
-  pinMode(13, OUTPUT);
+  pinMode(pinBuzzer, OUTPUT);
 }
 
 void loop()
 {
   // read the pushbutton input pin
   buttonState = digitalRead(pinButton);
+  
+  // Leo's hack
+  if (firstTime) {
+  	// buttonState = LOW;
+    Serial.println("first time: off");
+    noTone(pinBuzzer);  
+  }
+  
   // compare the buttonState to its previous state
   if (buttonState != lastButtonState) {
+    firstTime = false;
+    
     // if the state has changed, increment the counter
     if (buttonState == HIGH) {
       // if the current state is HIGH, then the button
@@ -64,24 +83,19 @@ void loop()
       Serial.println("on");
       Serial.print("number of button pushes: ");
       Serial.println(buttonPushCounter);
+      tone(pinBuzzer, 400);
     } else {
       // if the current state is LOW, then the button
       // went from on to off
       Serial.println("off");
+      noTone(pinBuzzer);
     }
     // delay a little bit to avoid debouncing
     delay(5); // Wait for 5 millisecond(s)
   }
+  
   // save the current state as the last state, for
   // the next time through the loop
   lastButtonState = buttonState;
-  // turns on the LED every four button pushes by
-  // checking the modulo of the button push counter.
-  // the modulo function gives you the remainder of
-  // the devision of two numbers
-  if (buttonPushCounter % 4 == 0) {
-    digitalWrite(pinLED, HIGH);
-  } else {
-    digitalWrite(pinLED, LOW);
-  }
+
 }
